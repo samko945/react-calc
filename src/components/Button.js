94f9cf535c2687e2ecfb2f30b96f7cea22c00b95
Samuel <samkoj@hotmail.co.uk>
2021-07-18 17:53:50 +0100

    update css

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,5 @@ import "./Button.css";
 
 export default function Button(props) {
 	const isButtonLarge = props.size === "double";
-	return (
-		<button className={`${isButtonLarge ? "size-double" : ""}`} style={{ backgroundColor: props.color }}>
-			{props.label}
-		</button>
-	);
+	return <button className={`${isButtonLarge ? "size-double" : ""} ${props.color}`}>{props.label}</button>;
 }
--- src/components/ButtonGrid.js
+++ src/components/ButtonGrid.js
@@ -2,43 +2,37 @@ import React from "react";
 import Button from "./Button";
 import "./ButtonGrid.css";
 
-const color = {
-	basePrimary: "",
-	baseSecondary: "#616161",
-	accent: "",
-};
-
 export default function ButtonGrid() {
 	return (
 		<div>
 			<div className="button-row">
-				<Button label="AC" color={color.baseSecondary} />
-				<Button label="+/-" color={color.baseSecondary} />
-				<Button label="%" color={color.baseSecondary} />
-				<Button label="/" color={color.accent} />
+				<Button label="AC" color="col-s2" />
+				<Button label="+/-" color="col-s2" />
+				<Button label="%" color="col-s2" />
+				<Button label="/" color="col-p1" />
 			</div>
 			<div className="button-row">
-				<Button label="7" color={color.basePrimary} />
-				<Button label="8" color={color.basePrimary} />
-				<Button label="9" color={color.basePrimary} />
-				<Button label="*" color={color.accent} />
+				<Button label="7" color="col-s1" />
+				<Button label="8" color="col-s1" />
+				<Button label="9" color="col-s1" />
+				<Button label="*" color="col-p1" />
 			</div>
 			<div className="button-row">
-				<Button label="4" color={color.basePrimary} />
-				<Button label="5" color={color.basePrimary} />
-				<Button label="6" color={color.basePrimary} />
-				<Button label="-" color={color.accent} />
+				<Button label="4" color="col-s1" />
+				<Button label="5" color="col-s1" />
+				<Button label="6" color="col-s1" />
+				<Button label="-" color="col-p1" />
 			</div>
 			<div className="button-row">
-				<Button label="1" color={color.basePrimary} />
-				<Button label="2" color={color.basePrimary} />
-				<Button label="3" color={color.basePrimary} />
-				<Button label="+" color={color.accent} />
+				<Button label="1" color="col-s1" />
+				<Button label="2" color="col-s1" />
+				<Button label="3" color="col-s1" />
+				<Button label="+" color="col-p1" />
 			</div>
 			<div className="button-row">
-				<Button size="double" label="0" />
-				<Button label="." color={color.basePrimary} />
-				<Button label="=" color={color.accent} />
+				<Button size="double" label="0" color="col-s1" />
+				<Button label="." color="col-s1" />
+				<Button label="=" color="col-p1" />
 			</div>
 		</div>
 	);
